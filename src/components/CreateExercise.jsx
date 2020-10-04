a74899b2f07da0ddf2d3106ffdb13a68c2f89e50
ParthArora <partharora.india@gmail.com>
2020-10-04 17:34:18 +0530

    Frontend Completed

--- src/components/CreateExercise.jsx
+++ src/components/CreateExercise.jsx
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 import DatePicker from 'react-datepicker'
+import axios from 'axios';
 
 
 export default class CreateExercise extends Component {
@@ -25,9 +26,12 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount(){
-        this.setState({
-            users : ['Parth'],
-            username : 'Parth'
+      axios.get("http://localhost:5000/users")
+        .then(res => {
+          this.setState({
+            users : res.data.map(user => user.username),
+            username : res.data[0].username
+          })
         })
     }
 
@@ -65,7 +69,8 @@ export default class CreateExercise extends Component {
             date : this.state.date
         }
 
-        console.log(exercise)
+        axios.post("http://localhost:5000/exercises/add", exercise)
+          .then(res => console.log(res.data))
 
         window.location = '/'
     }
